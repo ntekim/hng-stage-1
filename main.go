fcd2784ad93a69002303626c90de0a09cf2bb999
Ntekim-Jotham <jothamntekim@gmail.com>
2023-09-10 03:26:24 +0100

    Format the response data

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 
@@ -15,7 +14,7 @@ import (
 
 var port = "81"
 
-type response struct{
+type responseData struct{
 	Slack_name  		string
 	Track 				string
 	Current_day 		string
@@ -25,28 +24,37 @@ type response struct{
 	Status_code 		uint
 }
 
+type Response struct{
+	Error 	bool
+	Message string
+	Data	any
+}
+
 func GetJSONData(w http.ResponseWriter, r *http.Request) {
 	username := r.URL.Query().Get("slack_name")
 	track	 := r.URL.Query().Get("track")
 	
 	if len(username) == 0 || len(track) == 0 {
-		fmt.Println("Slack_name and track cannot be empty")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+
+		var respData Response
+		respData.Error = true
+		respData.Message = "Slack_name and track cannot be empty"
+		respData.Data = responseData{}
+
+		resp, err := json.Marshal(respData)
+		if err != nil {
+			panic(err)
+		}
+		_, err = w.Write(resp)
+		if err != nil {
+			panic(err)
+		}
 		return
 	}
 
-	var response response
-
-	executable_file, err := os.Executable()
-
-	if err != nil{
-		panic(err)
-	}
-
-	wd, err := os.Getwd()
-	if err != nil{
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	var response responseData
 
 	utc_time := time.Now().UTC().String()
 
@@ -54,23 +62,28 @@ func GetJSONData(w http.ResponseWriter, r *http.Request) {
 	timeSplit := strings.Split(utcArr[1], ".")
 	formattedTime := utcArr[0] + "T" + timeSplit[0] + "Z"
 
-	fmt.Println(wd)
+
 	response.Slack_name = username
 	response.Track = track
 	response.Current_day = time.Now().Weekday().String()
 	response.Utc_time = formattedTime
-	response.Github_file_url = executable_file
+	response.Github_file_url = "https://github.com/ntekim/hng-stage-1/blob/main/main.go"
 	response.Github_repo_url = "https://github.com/ntekim/hng-stage-1"
 	response.Status_code   = http.StatusOK
 
-	resp, err := json.Marshal(response)
+	var respData = Response{
+		Error: false,
+		Message: "Success",
+		Data: response,
+	}
+
+	resp, err := json.Marshal(respData)
 	if err != nil {
 		panic(err)
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-
 	_, err = w.Write(resp)
 	if err != nil {
 		panic(err)
@@ -91,7 +104,7 @@ func route() http.Handler{
 	}))
 
 	mux.Use(middleware.Logger)
-	mux.Get("/api", GetJSONData)
+	mux.Get("/", GetJSONData)
 
 	return mux
 }
